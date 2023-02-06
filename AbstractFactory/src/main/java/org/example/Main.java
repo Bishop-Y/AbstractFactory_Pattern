2b1034d03dfa3d17b387becf439b92e41a37864d
Bishop-Y <66753948+Bishop-Y@users.noreply.github.com>
2023-02-06 17:18:53 +0300

    Update Main.java

--- AbstractFactory/src/main/java/org/example/Main.java
+++ AbstractFactory/src/main/java/org/example/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         ComputerFactory appleFactory = new AppleComputerFactory();
         Computer appleComputer = appleFactory.createComputer();
-        Accessory apppleAccessory = appleFactory.createAccessory();
+        Accessory appleAccessory = appleFactory.createAccessory();
 
         ComputerFactory asusFactory = new AsusComputerFactory();
         Computer asusComputer = asusFactory.createComputer();
         Accessory asusAccessory = asusFactory.createAccessory();
 
         appleComputer.getModelOfComputer();
-        apppleAccessory.getNameOfAccessory();
+        appleAccessory.getNameOfAccessory();
         System.out.println();
         asusComputer.getModelOfComputer();
         asusAccessory.getNameOfAccessory();
